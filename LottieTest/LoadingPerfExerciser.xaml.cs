bf785ff6f51751a727ef421a97a99e87adf82c64
Simeon Cran <simeonc@microsoft.com>
2018-09-18 17:10:48 -0700

    Refactor the loading perf exerciser a bit to help find leakage.

--- LottieTest/LoadingPerfExerciser.xaml.cs
+++ LottieTest/LoadingPerfExerciser.xaml.cs
@@ -1,49 +1,50 @@
-﻿using Lottie;
-using Microsoft.UI.Xaml.Controls.CompositionPlayer;
+﻿using Microsoft.UI.Xaml.Controls.CompositionPlayer;
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace LottieTest
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class LoadingPerfExerciser : Page
     {
         public LoadingPerfExerciser()
         {
             this.InitializeComponent();
-            ExerciseLoading();
         }
 
-        async void ExerciseLoading()
+        async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ((Button)sender).IsEnabled = false;
+            await DoExerciseLoading();
+            ((Button)sender).IsEnabled = true;
+        }
+
+        async Task DoExerciseLoading()
+        {
+            await ExerciseLoading();
+            for (var i = 0; i < 3; i++)
+            {
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+            }
+        }
+
+        async Task ExerciseLoading()
         {
 
-            while (true)
+            for (var i = 0; i < 1000; i++)
             {
                 // Wait for the load. This does not wait for the instantiation.
-                await lottieSource.SetSourceAsync(new Uri("ms-appx:///assets/LottieLogo1.json"));
+                //                await lottieSource.SetSourceAsync(new Uri("ms-appx:///assets/LottieLogo1.json"));
+                player.Source = new Compositions.Empty_box();
 
                 // Wait for the lottie to load.
                 await WaitForCompositionLoadChange(true);
 
                 // Set the source to null to force unload.
-                await lottieSource.SetSourceAsync((Uri)null);
+                //await lottieSource.SetSourceAsync((Uri)null);
+                player.Source = null;
 
                 // Wait for the lottie to unload.
                 await WaitForCompositionLoadChange(false);
