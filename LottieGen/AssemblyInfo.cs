f81cbd456928d976a120f07b38d35409c1b1f38e
Simeon Cran <simeonc@microsoft.com>
2018-10-09 17:27:06 -0700

    Adding a sample that use the LottieCompositionSource referencing a private WinUI build.

--- LottieGen/AssemblyInfo.cs
+++ LottieGen/AssemblyInfo.cs
@@ -8,8 +8,8 @@
 //------------------------------------------------------------------------------
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.1.100.13463")]
-[assembly: AssemblyFileVersion("0.1.100.13463")]
+[assembly: AssemblyVersion("0.1.100.17275")]
+[assembly: AssemblyFileVersion("0.1.100.17275")]
 [assembly: AssemblyProduct("Lottie Windows")]
 [assembly: AssemblyCopyright("Microsoft 2018")]
 [assembly: AssemblyDescription("Command line Lottie code generator tool")]
